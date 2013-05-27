1a551e60786c024ba408cb7730c89c2f32c1c5a9
José Ibáñez <magic.traveler.gau@gmail.com>
2013-05-27 12:21:17 +0200

    Actualización 2
    Mejora de la consulta bases de datos.

--- JLATeam/JLATeam/Form3.Designer.cs
+++ JLATeam/JLATeam/Form3.Designer.cs
@@ -28,14 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackgroundImage = global::JLATeam.Properties.Resources.xzibit;
-            this.button1.Location = new System.Drawing.Point(721, 466);
+            this.button1.Location = new System.Drawing.Point(873, 643);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(112, 136);
             this.button1.TabIndex = 1;
@@ -45,17 +51,78 @@
             // label1
             // 
             this.label1.Image = global::JLATeam.Properties.Resources.baseMotorblog;
-            this.label1.Location = new System.Drawing.Point(12, 252);
+            this.label1.Location = new System.Drawing.Point(9, 664);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(350, 350);
+            this.label1.Size = new System.Drawing.Size(124, 115);
             this.label1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(787, 539);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 50);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Noticias";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(548, 539);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 50);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Fuentes";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(277, 539);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 50);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Comentarios";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(47, 539);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 50);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Autores";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(47, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(890, 481);
+            this.label2.TabIndex = 10;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(845, 611);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(984, 812);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form3";
@@ -68,5 +135,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- JLATeam/JLATeam/Form3.cs
+++ JLATeam/JLATeam/Form3.cs
@@ -8,13 +8,40 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using MySql.Data.MySqlClient;
+using MySql.Data.Types;
+
 namespace JLATeam
 {
     public partial class Form3 : Form
     {
+        //La línea que guarda la IP del servidor MySql, el usuario y la pass
+        String cadenaConexion;
+
+        //Conector que almacena la conexión a la BBDD
+        MySqlConnection conexion;
+
+        //Comando que quiero que se ejecute
+        MySqlCommand comando;
+
+        //Consulta
+        String sentenciaSQL;
+
+        //Resultado de la consulta
+        MySqlDataReader resultado;
+
+
         public Form3()
         {
             InitializeComponent();
+            try
+            {
+                cadenaConexion = "Server = localhost; Database = motorblog; Uid = root; Pwd = root ; Port = 3306;";
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+            }
+            catch (Exception) { }
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,6 +49,64 @@ namespace JLATeam
             Form1 v1 = new Form1();
             this.Hide();
             v1.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {//Botón para ver todos los equipos
+            sentenciaSQL = "SELECT * FROM autores;";
+            //sentenciaSQL = "SELECT id_autor, login, password, correo, url FROM autores;";
+            comando = new MySqlCommand(sentenciaSQL, conexion);
+            resultado = comando.ExecuteReader();
+            //MySqlDataAdapter adap = new MySqlDataAdapter();
+            //DataSet ds = new DataSet();
+            //Metemos un if para que pregunte si hemos presionado el boton nos muestre
+            //El resultado que pedimos en la consulta.
+            // Si metemos un if nos sale el ultimo de la lista?.
+            while (resultado.Read())
+            {
+                label2.Text += resultado.GetString("id_autor") + " " + resultado.GetString("login") + " " + resultado.GetString("Password");
+                //comboBox1.Items.Add(resultado.GetString("id_autor"));
+                
+                //DataTable tabla = new DataTable();
+                //adptLibros.Fill(tabla);
+
+            }
+            conexion.Close();
+            conexion.Open();
+            comando = new MySqlCommand(sentenciaSQL, conexion);
+            comando.ExecuteNonQuery();
+            //label1.Text = sentenciaSQL;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sentenciaSQL = "SELECT id_comentario, noticia FROM equipos WHERE puntos = (SELECT MAX(puntos) FROM liga.equipos);";
+            comando = new MySqlCommand(sentenciaSQL, conexion);
+            resultado = comando.ExecuteReader();
+            conexion.Close();
+            conexion.Open();
+            comando = new MySqlCommand(sentenciaSQL, conexion);
+            comando.ExecuteNonQuery();
+            label2.Text = sentenciaSQL;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sentenciaSQL = "SELECT nombre, puntos FROM equipos WHERE puntos = (SELECT MIN(puntos) FROM liga.equipos);";
+            comando = new MySqlCommand(sentenciaSQL, conexion);
+            resultado = comando.ExecuteReader();
+            conexion.Close();
+            conexion.Open();
+            comando = new MySqlCommand(sentenciaSQL, conexion);
+            comando.ExecuteNonQuery();
+            label2.Text = sentenciaSQL;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
